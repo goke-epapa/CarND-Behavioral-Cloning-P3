f20a1a5412be3a132d0eb49ca399313ea16653f7
Adegoke Obasa <adegokeobasa@gmail.com>
2019-07-08 06:46:14 +0000

    create nvidia SDC model

---
+++ model.py
@@ -0,0 +1,53 @@
+import csv
+import cv2
+from scipy import ndimage
+import numpy as np
+
+lines = []
+with open('./data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    count = 0
+    for line in reader:
+        if count != 0:
+            lines.append(line)
+            count += 1
+        
+images = []
+measurements = []
+
+for line in lines:
+    source_path = line[0]
+    print(source_path)
+    filename = source_path.split('/')[-1]
+    current_path = './data/IMG/' + filename
+    print(current_path)
+    
+    image = ndimage.imread(current_path)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement)
+    
+x_train = np.array(images)
+y_train = np.array(measurements)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers.convolutional import Conv2D
+
+model = Sequential()
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(Conv2D(24, (5, 5), activation="relu", strides=(2, 2)))
+model.add(Conv2D(36, (5, 5), activation="relu", strides=(2, 2)))
+model.add(Conv2D(48, (5, 5), activation="relu", strides=(2, 2)))
+model.add(Conv2D(64, (3, 3), activation="relu"))
+model.add(Conv2D(64, (3, 3), activation="relu"))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(x_train, y_train, validation_split=0.2, shuffle=True)
+
+model.save('model.h5')
