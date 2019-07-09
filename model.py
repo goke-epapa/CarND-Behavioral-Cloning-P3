4126520ee0e792803ce677e6d26e9c6df1dfb29f
Adegoke Obasa <adegokeobasa@gmail.com>
2019-07-09 06:01:08 +0000

    augment training data

--- model.py
+++ model.py
@@ -24,10 +24,19 @@ for line in lines:
     images.append(image)
     measurement = float(line[3])
     measurements.append(measurement)
+    
+# Augment images
+augmented_images, augmented_measurements = [], []
+for image, measurement in zip(images, measurements):
+    augmented_images.append(image)
+    augmented_measurements.append(measurement)
+    augmented_images.append(cv2.flip(image, 1))
+    augmented_measurements.append(measurement * -1.0)
 
-x_train = np.array(images)
-y_train = np.array(measurements)
+x_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
 
+# Nvidia Self Driving Car Model 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Conv2D
@@ -47,6 +56,6 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(x_train, y_train, validation_split=0.2, shuffle=True)
+model.fit(x_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
 
 model.save('model.h5')
