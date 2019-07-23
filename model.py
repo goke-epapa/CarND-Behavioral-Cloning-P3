3137dc36c67b52715dc4462287d9774c68ba86b5
Adegoke Obasa <adegokeobasa@gmail.com>
2019-07-23 23:53:13 +0000

    add dropout

--- model.py
+++ model.py
@@ -48,13 +48,13 @@ images, measurements = load_raw_data(samples)
 # Augment images by flipping images and multiplying measurements by -1
 augmented_images, augmented_measurements = augment_images(images, measurements)
 
-x_train = np.array(augmented_images)
-y_train = np.array(augmented_measurements)
+x_train = np.array(images)
+y_train = np.array(measurements)
 
 
 # Nvidia Self Driving Car CNN
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Conv2D
 
 model = Sequential()
@@ -67,6 +67,7 @@ model.add(Conv2D(64, (3, 3), activation="relu"))
 model.add(Conv2D(64, (3, 3), activation="relu"))
 model.add(Flatten())
 model.add(Dense(100))
+model.add(Dropout(0.5))
 model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
@@ -74,7 +75,7 @@ model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
 
 # Train model
-model.fit(x_train, y_train, validation_split=0.2, shuffle=True, epochs=3)
+model.fit(x_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
 
 # Save model to file
 model.save('model.h5')
